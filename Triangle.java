f0ca193c351fe1d01416b891a749d41ff3b5b75c
Stephanie Siaw <siaw.stephanie@gmail.com>
2015-06-14 13:37:27 -0400

    Added Triangle class and empty triangle methods

---
+++ Triangle.java
@@ -0,0 +1,21 @@
+public class Triangle {
+    public static void main (String[] args) {
+        Triangle t = new Triangle();
+        t.printAsterisksHorizontally(1);
+        t.printAsterisksHorizontally(8);
+        t.printAsterisksVertically(3);
+        o.printRightTriangle(3);
+    }
+
+    void printAsterisksHorizontally() {
+
+    }
+
+    void printAsterisksVertically() {
+
+    }
+
+    void printRightTriangle() {
+
+    }
+}
