6dca6045aa11738ad6137346db44a574d5cba1a5
Stephanie Siaw <siaw.stephanie@gmail.com>
2015-06-16 07:53:52 -0700

    Finished first diamond function

---
+++ Diamond.java
@@ -0,0 +1,20 @@
+public class Diamond {
+    public static void main (String[] args) {
+        Diamond d = new Diamond();
+        d.printHalfDiamond(4);
+    }
+
+    void printHalfDiamond(int num) {
+        for(int row = 0; row < num; row++) {
+            StringBuilder rowString = new StringBuilder();
+            int rowLength = 2 * num - 1;
+            for (int space = 0; space < (num - 1 - row); space++) {
+                rowString.append(" ");
+            }
+            for (int asterix = 0; asterix < (2 * row + 1); asterix++) {
+                rowString.append("*");
+            }
+            System.out.println(rowString.toString());
+        }
+    }
+}
